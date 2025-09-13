01cff406a7840c9d112c6876bfa525438a23db93
ryansoe <ryansoe90686@gmail.com>
2025-09-12 20:47:45 -0700

    feat: heading and subheading change

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -5,9 +5,7 @@ import ProjectCard from "./ProjectCard";
 const ProjectSection = () => (
   <section className="my-16 px-4">
     <div className="max-w-full sm:max-w-[95%] lg:max-w-[66.67%] mx-auto">
-      <h2 className="text-2xl font-bold uppercase underline mb-5 text-center">
-        Projects
-      </h2>
+      <h2 className="text-2xl font-bold  mb-5 text-center">Projects</h2>
       <div className="space-y-5">
         {projects.map((project) => (
           <ProjectCard key={project.id} {...project} />
