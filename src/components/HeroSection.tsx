0d2c78f61c1fc0ae0e8734e5b6277712650c1080
ryansoe <ryansoe90686@gmail.com>
2025-09-13 15:16:51 -0700

    feat: changed spacing

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -41,13 +41,13 @@ const HeroSection = () => {
   }, []);
 
   return (
-    <section className="my-16 px-4">
+    <section className="my-12 px-4">
       <div className="max-w-full sm:max-w-[95%] lg:max-w-[66.67%] mx-auto text-left">
-        <h1 className="text-5xl font-medium mb-6 text-black">
+        <h1 className="text-5xl font-medium mb-2 text-black">
           {personalInfo.name}
         </h1>
 
-        <p className="text-xl font-medium text-black mb-6 leading-relaxed">
+        <p className="text-xl font-medium text-black mb-4 leading-relaxed">
           {personalInfo.description}
         </p>
 
