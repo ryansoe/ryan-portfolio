681149be21a690e626a0fc22ab98c74b35aeb0a1
ryansoe <ryansoe26@gmail.com>
2025-11-29 14:06:11 -0800

    increase space between link and underline

--- src/components/ui/animated-underline.tsx
+++ src/components/ui/animated-underline.tsx
@@ -11,7 +11,7 @@ export const AnimatedUnderline = ({
 }: AnimatedUnderlineProps) => {
   return (
     <span
-      className={`relative inline-block after:absolute after:bg-black after:bottom-0 after:left-0 after:h-px after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300 ${className}`}
+      className={`relative inline-block after:absolute after:bg-black after:-bottom-0.5 after:left-0 after:h-px after:w-full after:origin-bottom-right after:scale-x-0 hover:after:origin-bottom-left hover:after:scale-x-100 after:transition-transform after:ease-in-out after:duration-300 ${className}`}
     >
       {children}
     </span>
